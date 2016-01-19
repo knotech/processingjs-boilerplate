99837e3849b550a218d8488075bd894a19ca6fcc
Gabriel Schroder <knotech@users.noreply.github.com>
2016-01-19 06:56:07 -0600

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,10 @@ app.get('/', function(req, res) {
 	res.send('shit is moving');
 });
 
+// TODO: Add config file for dynamic port allocation based on env
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('shit is listening at http://%s:%s.', host, port);
+	console.log('Server is listening at http://%s:%s.', host, port);
 });
